2e0f5440c8a271c610f8f2f001594b4f89b12dfa
Austin Luft <ausluft@gmail.com>
2018-10-10 20:27:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,21 @@
 class Song
+
+@@count = 0
   
 attr_accessor :name, :artist, :genre
   
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@ccount += 1
+end
   
-  
-  
+def count
+  @@count
+end
+
+
   
   
 end #class Song
